6d171d46ed00b635a7b64764dabc0190583e12f9
jurianoff <r.jurylik@gmail.com>
2022-06-16 23:08:46 +0200

    Add hover efect to divs

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const grid = document.querySelector(".grid");
 
 for (let i = 1; i <= 16*16; i++) {
     const square = document.createElement('div');
-    square.textContent = (i);
     square.classList.add('square');
     grid.appendChild(square);
+    square.addEventListener('mouseover', () => {
+    square.classList.add('hover');
+    })
 };
