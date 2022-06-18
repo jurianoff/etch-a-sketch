df0c7619c2bc57a5e9ed65b33478bdbe0904f79d
jurianoff <r.jurylik@gmail.com>
2022-06-18 23:20:38 +0200

    Add black mode

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ for (let i = 1; i <= gridSize*gridSize; i++) {
     })
 };
 
-const size = document.querySelector('.size');
-size.addEventListener('click', () => {
+function clear () {
     for (let i = 1; i <= gridSize*gridSize; i++) {
         oldSquares = document.querySelector('.square');
         grid.removeChild(oldSquares);
-    }
+}};
+
+const size = document.querySelector('.size');
+size.addEventListener('click', () => {
+    clear();
 
     while(true) {
         gridSize = prompt("Set grid size. Max:100");
@@ -30,13 +33,41 @@ size.addEventListener('click', () => {
         const square = document.createElement('div');
         square.classList.add('square');
         grid.appendChild(square);
-        let squaresToStyle = document.getElementsByClassName('square');
-        for (let i = 0; i < squaresToStyle.length; i++) {
-            squaresToStyle[i].style.flexBasis = `${720/gridSize}px`;
-        }
-        console.log(squaresToStyle);
+        
         square.addEventListener('mouseover', () => {
         square.classList.add('hover');
         })
         };
+    let squaresToStyle = document.getElementsByClassName('square');
+    for (let i = 0; i < squaresToStyle.length; i++) {
+        squaresToStyle[i].style.flexBasis = `${720/gridSize}px`;
+    }
+});
+
+const blackMode = document.querySelector('.black');
+blackMode.addEventListener('click', () => {
+    clear();
+    for (let i = 1; i <= gridSize*gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+        square.addEventListener('mouseover', () => {
+        square.classList.add('blackMode');
+        })}
+    let squaresToStyle = document.getElementsByClassName('square');
+    for (let i = 0; i < squaresToStyle.length; i++) {
+        squaresToStyle[i].style.flexBasis = `${720/gridSize}px`;
+    };
+    });
+
+const rainbowMode = document.querySelector('.rainbow');
+rainbowMode.addEventListener('click', () => {
+    clear();
+    for (let i = 1; i <= gridSize*gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+        square.addEventListener('mouseover', () => {
+        square.classList.add('rainbowMode');
+        })}
 });
