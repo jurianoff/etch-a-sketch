bdee0dc8caf7758293f35b2f7f0ad049d7422281
jurianoff <r.jurylik@gmail.com>
2022-06-18 22:23:43 +0200

    Add grid change functionality

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const grid = document.querySelector(".grid");
-
-for (let i = 1; i <= 16*16; i++) {
+let gridSize = 16;
+for (let i = 1; i <= gridSize*gridSize; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
     grid.appendChild(square);
@@ -8,3 +8,35 @@ for (let i = 1; i <= 16*16; i++) {
     square.classList.add('hover');
     })
 };
+
+const size = document.querySelector('.size');
+size.addEventListener('click', () => {
+    for (let i = 1; i <= gridSize*gridSize; i++) {
+        oldSquares = document.querySelector('.square');
+        grid.removeChild(oldSquares);
+    }
+
+    while(true) {
+        gridSize = prompt("Set grid size. Max:100");
+        if (gridSize === null) {
+            break;
+        }else if (!isNaN(gridSize) && gridSize <= 100 && gridSize >= 1) {
+            break;
+        };
+        parseInt(gridSize);
+        };
+
+    for (let i = 1; i <= gridSize*gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+        let squaresToStyle = document.getElementsByClassName('square');
+        for (let i = 0; i < squaresToStyle.length; i++) {
+            squaresToStyle[i].style.flexBasis = `${720/gridSize}px`;
+        }
+        console.log(squaresToStyle);
+        square.addEventListener('mouseover', () => {
+        square.classList.add('hover');
+        })
+        };
+});
