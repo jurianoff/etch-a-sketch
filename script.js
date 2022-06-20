0a78ea40328f317e184ecff5da3e8bd12b566542
jurianoff <r.jurylik@gmail.com>
2022-06-20 22:19:52 +0200

    Add rainbow mode

--- script.js
+++ script.js
@@ -67,7 +67,14 @@ rainbowMode.addEventListener('click', () => {
         const square = document.createElement('div');
         square.classList.add('square');
         grid.appendChild(square);
+        let h = Math.floor((Math.random() * 360) + 1);
+        let s = 100;
+        let l = 50;
         square.addEventListener('mouseover', () => {
-        square.classList.add('rainbowMode');
+        square.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
         })}
+    let squaresToStyle = document.getElementsByClassName('square');
+    for (let i = 0; i < squaresToStyle.length; i++) {
+        squaresToStyle[i].style.flexBasis = `${720/gridSize}px`;
+    };
 });
